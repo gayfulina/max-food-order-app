2645e4fe0c5951629faf7719a22b552d8bbdffd6
gayfulina <irinmdr@gmail.com>
2021-06-15 16:01:21 -0700

    input

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,14 @@
+import classes from './Modal.module.css';
+
+const Modal = props => {
+    return (
+        <>
+            <div className={classes.backdrop}/>
+            <div className={classes.modal}>
+            {props.children}
+            </div>
+        </>
+    )
+}
+
+export default Modal;
