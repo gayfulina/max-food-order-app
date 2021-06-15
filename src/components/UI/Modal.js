6cacba504f6e5bb5b32ec758e45394382060e9b9
gayfulina <irinmdr@gmail.com>
2021-06-15 16:13:40 -0700

    input

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,8 @@
+import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = props => {
-    return  <div className={classes.backdrop}/>
+    return <div className={classes.backdrop}/>
 };
 
 const ModalOverlay = props => {
@@ -12,11 +13,16 @@ const ModalOverlay = props => {
     )
 };
 
+const portalElement = document.getElementById("overlays")
+
 const Modal = props => {
     return (
         <>
-            <Backdrop/>
-            <ModalOverlay/>
+            {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
         </>
     )
 }
